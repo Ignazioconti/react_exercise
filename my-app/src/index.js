0798ca772005b1d1ce42232e273d9dc009bc3830
ignazioconti <ignazioconti@gmail.com>
2022-11-14 17:05:05 +0100

    svolgimento

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,3 +3,9 @@ const hello = <h1>"Hello Word"</h1>;
 function hello(name) {
   return <h1>'Hello, {name}</h1>;
 }
+
+// ---------------JSX-05-----------------//
+
+function sum(a, b) {
+  return <h2>${a + b}</h2>;
+}
