9798925160f3f72885fdbfecb090bdeda1bf904f
ignazioconti <ignazioconti@gmail.com>
2022-11-19 15:26:25 +0100

    svolgimento

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { HelloWord } from "./component-02";
-class App extends React.Component {
+import { HelloWord } from "./Component-02";
+export class App extends React.Component {
   render() {
     return (
       <div>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,5 @@
-// const hello = <h1>"Hello Word"</h1>;
-
-// function hello(name) {
-//   return <h1>'Hello, {name}</h1>;
-// }
-
-// // ---------------JSX-05-----------------//
-
-// function sum(a, b) {
-//   return <h2>${a + b}</h2>;
-// }
 import ReactDOM from "react-dom";
-import { HelloWord } from "./component-02";
-const helloWordElement = <HelloWord />;
+import { App } from "./App";
 const root = document.querySelector("#root");
 
-ReactDOM.render(helloWordElement, root);
+ReactDOM.render(<App />, root);
