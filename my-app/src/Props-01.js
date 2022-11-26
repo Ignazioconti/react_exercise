6df498e91e2a975910592b2cfb1f5c6c5ad9df3d
ignazioconti <ignazioconti@gmail.com>
2022-11-26 18:34:56 +0100

    correzione

--- my-app/src/Props-01.js
+++ my-app/src/Props-01.js
@@ -4,9 +4,12 @@ export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        {!!this.props.name && <p>Welcome, {this.props.name}</p>}
-        {!this.props.name && <p>Welcome!!!!</p>}
+        <p>Welcome, {this.props.name}</p>
       </div>
     );
   }
 }
+
+Welcome.defaultProps = {
+  name: "Ciao",
+};
--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { HelloWord } from "./Component-02";
-import { Message } from "./Component-07";
 import { Welcome } from "./Props-01";
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <HelloWord /> <Message />
-        <p>
-          <Welcome />
-        </p>
+        <HelloWord />
+
+        <Welcome />
       </div>
     );
   }
